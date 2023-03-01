6218c2a0f84dbd304cf20c973e9176de7d0492a3
Peter Kionga-Kamau <13005927+Mugane@users.noreply.github.com>
2023-03-01 16:15:43 -0600

    Minor change to standardize variable name formats

--- auth.php
+++ auth.php
@@ -1,22 +1,23 @@
 <?php
-	// Login system for a single php file:
+	// Login system in a single php file.
+	// Tabs, obviously; Configurable editors exist for those with spacing hangups.
 	// ----------------- Begin Login Section (add protected content after this section) -----------------
 	@session_start();
-  		// --- start modifiable variables: ---
+ 		// --- start modifiable variables: ---
   		// $credentials contains unsalted hash for the login "admin" and "password" (replace with your hashed credentials):
 		// Generate a hash in bash: echo $(echo -n "texttohash" | sha256sum | cut -d " " -f1)
 	$credentials = array('8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918' => '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
 	$salt        = '';    // add random string here to salt your password hashes (vaguely more secure)
 	$max_logins  = 5;     // maximum number of failed login attempts before ban
 	$ban_time    = 24;    // ban hours
-	$timeout     = 180;   // number of seconds before session timeout (since last active)
+	$time_out    = 900;   // number of seconds before session timeout (since last active)
 	$max         = 86400; // maximum length of login (even with activity)
  		// --- end modifiable variables ---
 	$message     = ''; // placeholder for login error/status messages
 	if(@$_SESSION[$_COOKIE['PHPSESSID']]['auth']['ban_time'] && $_SESSION[$_COOKIE['PHPSESSID']]['auth']['ban_time'] > time()) exit; // this is not very secure
 	if(isset($_GET["logout"])) LOGGED_IN(0); // log out
 	if(isset($_REQUEST["login"])) LOG_IN(@$_POST['login'],@$_POST['password']); // log in
-	if(!LOGGED_IN($timeout, $max)) SHOW_LOGIN();
+	if(!LOGGED_IN($time_out, $max)) SHOW_LOGIN();
 	function LOG_IN($u,$p) {
 		global $credentials,$max_logins,$ban_time;
 		unset($_SESSION[$_COOKIE['PHPSESSID']]['auth']['fail_count']);
@@ -34,24 +35,24 @@
 	}
 	function SHOW_LOGIN() {
 		exit ('<html><head><style class="text/css">
-				 body { margin: 0; display: flex; background: linear-gradient(to right, rgba(117,189,209,0.5) 0%, rgba(193,234,191,0.7) 100%), linear-gradient(to bottom, rgba(147,206,222,0) 0%, rgba(117,189,209,1) 41%, rgba(73,165,191,0.6) 100%); }
-		         form { display: flexbox; margin: auto auto; vertical-align: middle; padding: 20px 30px; border-radius:10px; background: rgba(255,255,255,0.5); text-align: right; filter: drop-shadow(15px 10px 6px rgba(0,40,40,0.2)); } 
-				 p,input { display: block; font-family: sans-serif; margin: 0 auto; }
-				 input { margin: 5px 0px; padding: 5px 8px; }
-		         input[type=text],input[type=password],input[type=submit] { border: 1px solid rgba(0,0,0,0.4); width: 100%; }
-				 p,input[type=submit] { color: rgba(0,0,0,0.7); width: auto; } 
-				 input[type=submit] { margin-left: auto; margin-right: 0; padding: 5px 25px; } 
-		        </style></head>
-		        <body><form method="POST" name="login">
-				 <p>'.$_SESSION[$_COOKIE['PHPSESSID']]['auth']['message'].'</p>
-				 <input type="hidden" name="login" value="login" /><input type="text" name="login" />
-			     <input type="password" name="password" /><input type="submit" value="Log in..." />
-			    </form></body></html>');
+			 body { margin: 0; display: flex; background: linear-gradient(to right, rgba(117,189,209,0.5) 0%, rgba(193,234,191,0.7) 100%), linear-gradient(to bottom, rgba(147,206,222,0) 0%, rgba(117,189,209,1) 41%, rgba(73,165,191,0.6) 100%); }
+			 form { display: flexbox; margin: auto auto; vertical-align: middle; padding: 20px 30px; border-radius:10px; background: rgba(255,255,255,0.5); text-align: right; filter: drop-shadow(15px 10px 6px rgba(0,40,40,0.2)); } 
+			 p,input { display: block; font-family: sans-serif; margin: 0 auto; }
+			 input { margin: 5px 0px; padding: 5px 8px; }
+			 input[type=text],input[type=password],input[type=submit] { border: 1px solid rgba(0,0,0,0.4); width: 100%; }
+			 p,input[type=submit] { color: rgba(0,0,0,0.7); width: auto; } 
+			 input[type=submit] { margin-left: auto; margin-right: 0; padding: 5px 25px; } 
+			</style></head>
+			<body><form method="POST" name="login">
+			  <p>'.$_SESSION[$_COOKIE['PHPSESSID']]['auth']['message'].'</p>
+			  <input type="hidden" name="login" value="login" /><input type="text" name="login" />
+			  <input type="password" name="password" /><input type="submit" value="Log in..." />
+			</form></body></html>');
 	}
-	function LOGGED_IN($timeout=180, $max=86400) {
+	function LOGGED_IN($time_out=180, $max=86400) {
 		if(@$_SESSION[$_COOKIE['PHPSESSID']]['auth']['login_time'] > 9999999 && 
-		   time() - @$_SESSION[$_COOKIE['PHPSESSID']]['auth']['last_seen'] < $timeout && 
-		   time() - @$_SESSION[$_COOKIE['PHPSESSID']]['auth']['login_time'] < $max ) {
+			time() - @$_SESSION[$_COOKIE['PHPSESSID']]['auth']['last_seen'] < $time_out && 
+			time() - @$_SESSION[$_COOKIE['PHPSESSID']]['auth']['login_time'] < $max ) {
 			$_SESSION[$_COOKIE['PHPSESSID']]['auth']['last_seen'] = time();
 			return true;
 		} else {
@@ -61,7 +62,7 @@
 			$_SESSION[$_COOKIE['PHPSESSID']]['auth']['url'] = $_SERVER['REQUEST_URI'];
 		}
 		unset($_SESSION[$_COOKIE['PHPSESSID']]['auth']['last_seen'], $_SESSION[$_COOKIE['PHPSESSID']]['auth']['login_time']);
-		if($timeout == 0) { header('location: '.$_SERVER['PHP_SELF']); exit; }
+		if($time_out == 0) { header('location: '.$_SERVER['PHP_SELF']); exit; }
 		return false;
 	}
 	// ----------------- End Login Section (add protected content after this line) -----------------
